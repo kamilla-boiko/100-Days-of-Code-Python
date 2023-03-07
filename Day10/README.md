8b222988d2cacac1ab666f2befa3a703dd0913c6
kamilla-boiko <kamilla.boyko@gmail.com>
2023-03-07 18:35:33 +0200

    simple calculator
    Signed-off-by: kamilla-boiko <kamilla.boyko@gmail.com>

---
+++ Day10/README.md
@@ -0,0 +1,12 @@
+# Day 10 Project: Calculator
+
+It's a simple text base calculator that supports plus, minus, multiply 
+or divide operations.
+
+It asks you to write the first number, then pick one of the operations 
+and also write the second number.
+
+Then it shows the full calculation, for example: 5 / 2 = 2.5.
+
+And now you can continue calculating with the answer from the previous step,
+or we type 'n' to start a new calculation.
---
+++ Day10/main.py
@@ -0,0 +1,74 @@
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    try:
+        return num1 / num2
+    except ZeroDivisionError:
+        print("You can't divide by zero")
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+
+def calculator():
+    print(logo)
+    num1 = float(input("What's the first number?: "))
+
+    for operation in operations:
+        print(operation)
+
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+
+        answer = operations[operation_symbol](num1, num2)
+
+        if answer == None:
+            break
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, "
+                 f"or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
