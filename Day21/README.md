c175bce9395abf94bd588303299df71a03b18e1f
Kamilla Boiko <kamilla.boyko@gmail.com>
2024-02-06 14:27:50 +0200

    full snake game

---
+++ Day21/README.md
@@ -0,0 +1,16 @@
+# Day 21 Project: Snake Game Part 2
+
+Step 4. Detect collision with food
+
+Step 5. Create a scoreboard
+
+Step 6. Detect collision with wall
+
+Step 7. Detect collision with tail
+
+It is a game where you control the snake using keyboard, and you can go and get food, 
+and then it grows in length as it eats more food.
+
+And you have to make sure that you don't end up getting tangled or hitting the wall.
+
+And the goal is to eat as much food as you can while staying alive.
---
+++ Day21/main.py
@@ -0,0 +1,63 @@
+from turtle import Screen
+import time
+
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+
+def play():
+    screen = Screen()
+    screen.setup(width=600, height=600)
+    screen.bgcolor("DarkOliveGreen1")
+    screen.title("Snake Game")
+    screen.tracer(0)
+
+    scoreboard = Scoreboard()
+    snake = Snake()
+    food = Food()
+
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
+    game_is_on = True
+    while game_is_on:
+        screen.update()
+        time.sleep(0.1)
+        snake.move()
+
+        # Eat the food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+
+        # Collision with wall
+        if snake.head.xcor() > 295 or snake.head.xcor() < -295 or snake.head.ycor() > 295 or snake.head.ycor() < -295:
+            game_is_on = False
+            scoreboard.game_over()
+
+        # Eat the tail
+        for segment in snake.segments[1:]:
+            if snake.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard.game_over()
+
+        # Retry the game
+        if not game_is_on:
+            answer = screen.textinput(
+                title="GAME OVER",
+                prompt=f"Your score is {scoreboard.score}!\nDo you want to restart? (y/n)"
+            )
+            if answer == "y":
+                screen.clearscreen()
+                play()
+
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    play()
